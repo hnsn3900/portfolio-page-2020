331b7596f80f2b8953d32bce283b498e936001cc
hnsn3900 <mshift1040@google.com>
2020-12-05 04:32:46 -0500

    Add toggleAccordianMenu function

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ let headerNav = document.querySelector(".header-nav");
 let headerNavBtn = document.querySelector(".header-nav-btn");
 
 // EVENT LISTENER
+// headerNavBtn.addEventListener(
+//   "click",
+//   toggleMenu.bind(null, headerNav, header)
+// );
+
 headerNavBtn.addEventListener(
   "click",
-  toggleMenu.bind(null, headerNav, header)
+  toggleAccordionMenu.bind(headerNavBtn, headerNav, header)
 );
+
 headerNavBtn.addEventListener("click", toggleMenuIcon.bind(headerNavBtn));
 
 // FUNCTIONS
@@ -26,3 +32,24 @@ function toggleMenu(menu, menuContainer) {
     menu.classList.add("expandMenu");
   }
 } // eof()
+
+function toggleAccordionMenu(menu, menuParent) {
+  const menuItems = menu.children[0].children.length; // Represents list items in unordered list element
+  const parentHeight = menuParent.clientHeight;
+  console.log("menuItems, parentHeight:", menuItems, parentHeight);
+
+  if (menu.clientHeight >= parentHeight) {
+    // Vertically transform menu from hidden overflow position. Expand height of menu's parent.
+    menu.style.transform = `translateY( ${parentHeight * menuItems}px)`;
+    menuParent.style.height = `${parentHeight * (menuItems + 1)}px`;
+    menu.style.transition = `transform 1s ease-in-out`;
+    menuParent.style.transition = `height 1s ease-in-out`;
+    menu.classList.add("menuOpened");
+  } else {
+    // Vertically transform menu to hidden overflow position. Shrink height of menu's parent.
+    menu.style.transform = `translateY(0px)`;
+    menuParent.style.height = `${parentHeight / (menuItems + 1)}px`;
+    menu.style.transition = `transform 0.8s ease-in-out`;
+    menuParent.style.transition = `height 0.8s ease-in-out`;
+  }
+}
